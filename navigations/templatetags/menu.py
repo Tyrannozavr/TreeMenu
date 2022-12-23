717dca8570556140951d0b513f49c71ce6aba873
dmiv <dmitriy40647274@gmail.com>
2022-12-23 23:43:16 +0300

    worked some example inclusion_tag

--- navigations/templatetags/menu.py
+++ navigations/templatetags/menu.py
@@ -2,7 +2,13 @@ from django import template
 
 register = template.Library()
 
-def menu(value, arg):
-    return value + arg
+@register.inclusion_tag('tags/menu.html')
+def draw_menu(value):
+    return value.capitalize()
 
-register.filter('menu', menu)
+
+@register.inclusion_tag('tags/results.html')
+def show_results(poll):
+    print(poll)
+    choices = poll
+    return {'choices': choices}
