313c641f911b4134205ff1dfc3cf3b231e0901d7
dmiv <dmitriy40647274@gmail.com>
2022-12-26 21:53:05 +0300

    simple render

--- navigations/templatetags/menu.py
+++ navigations/templatetags/menu.py
@@ -8,5 +8,32 @@ def draw_menu(value):
 
 @register.inclusion_tag('tags/results.html')
 def show_results(poll):
-    choices = poll
-    return {'choices': choices}
+    return {'choices': poll}
+
+@register.tag()
+def shownodes(parser, token):
+    nodelist = parser.parse(("endshownodes",))
+    parser.delete_first_token()
+    return ShowNodesNode(token, nodelist)
+
+class ShowNodesNode(template.Node):
+    def __init__(self, token, nodelist):
+        self.token = token
+        self.nodelist = nodelist
+
+    def render(self, context):
+        result = [
+            "<ul><li>Token info:</li><ul>",
+        ]
+
+        for part in self.token.split_contents():
+            content = str(part)
+            result.append(f"<li>{content}</li>")
+
+        result.append("</ul><li>Block contents:</li><ul>")
+        for node in self.nodelist:
+            content = str(node)
+            result.append(f"<li>{content}</li>")
+
+        result.append("</ul>")
+        return "".join(result)
