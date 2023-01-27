fc39819bf585014c4a1969474f424d6b5e40f01b
dmiv <dmitriy40647274@gmail.com>
2023-01-27 18:47:30 +0300

    in admin panel add iteminline

--- navigations/admin.py
+++ navigations/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
+from django import forms
 
 from .models import *
 
-admin.site.register(Menu)
+
+# admin.site.register(Menu)
+
+class ItemInline(admin.StackedInline):
+    model = Item
+    extra = 1
+    fields = ('name', 'parent')
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    inlines = (ItemInline,)
+    fields = ('name',)
 
 
 @admin.register(Item)
--- navigations/models.py
+++ navigations/models.py
@@ -16,7 +16,7 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     parent = models.ForeignKey('self', related_name='children_set', on_delete=models.CASCADE, null=True, blank=True)
     hierarchy = models.CharField(max_length=255, null=True, blank=True)  # this field is required to create single query
-                                                                          # in the DB, has the form el1:el2
+                                                                         # in the DB, has the form el1:el2
 
     def children(self):
         """
@@ -30,12 +30,10 @@ class Item(models.Model):
         else:
             return [None]
 
-
     def save(self, *args, **kwargs):
         tree = self.parents_tree()
         self.hierarchy = ':'.join([str(i) for i in tree if i])
         super(Item, self).save(*args, **kwargs)
-
 
     def __str__(self):
         return self.name
